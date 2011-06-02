3b87050bb06fcacc66a149e3645663843526c5e3
Friedrich Lindenberg <friedrich@pudo.org>
2011-06-02 12:10:26 +0200

    [harvesters] remove pdeuharvester and use harvesterbase from ckanext-harvester

--- ckanext/pdeu/harvesters.py
+++ ckanext/pdeu/harvesters.py
@@ -1,164 +1,21 @@
 #coding: utf-8
-import urllib2,urllib,urlparse
+import urllib2, urllib, urlparse
 import string
-import re
 from datetime import datetime
 
 import logging
 
-from ckan.logic.action.create import package_create_rest
-from ckan.logic.action.update import package_update_rest
-from ckan.logic.action.get import package_show
-from ckan.logic.schema import default_package_schema
 from ckan.logic import ValidationError, NotFound
-from ckan import model
-from ckan.model import Session,Package
-from ckan.lib.navl.validators import ignore_missing
-from ckan.lib.munge import munge_title_to_name
 from ckan.lib.helpers import json
 
-from ckan.plugins.core import SingletonPlugin, implements
-
-from ckanext.harvest.interfaces import IHarvester
-from ckanext.harvest.model import HarvestJob, HarvestObject, HarvestGatherError, \
-                                    HarvestObjectError
-
-from lxml import html,etree
+from ckanext.harvest.model import HarvestObject
+from ckanext.harvest.harvesters import HarvesterBase
+from lxml import html, etree
 from cookielib import CookieJar
 
 log = logging.getLogger(__name__)
 
-class PDEUHarvester(SingletonPlugin):
-    '''
-    Generic class for publicdata.eu harvesters
-    '''
-    implements(IHarvester)
-
-    def _gen_new_name(self,title):
-        name = munge_title_to_name(title).replace('_', '-')
-        while '--' in name:
-            name = name.replace('--', '-')
-        return name
-
-    def _check_name(self,name):
-        like_q = u'%s%%' % name
-        pkg_query = Session.query(Package).filter(Package.name.ilike(like_q)).limit(100)
-        taken = [pkg.name for pkg in pkg_query]
-        if name not in taken:
-            return name
-        else:
-            counter = 1
-            while counter < 101:
-                if name+str(counter) not in taken:
-                    return name+str(counter)
-                counter = counter + 1
-            return None
-
-    def _get_content(self, url):
-        http_request = urllib2.Request(url=url)
-        try:
-            http_response = urllib2.urlopen(http_request)
-            return http_response.read()
-        except Exception, e:
-            raise e
-
-    def _save_gather_error(self,message,job):
-        err = HarvestGatherError(message=message,job=job)
-        err.save()
-        log.error(message)
-
-    def _save_object_error(self,message,obj,stage=u'Fetch'):
-        err = HarvestObjectError(message=message,object=obj,stage=stage)
-        err.save()
-        log.error(message)
-
-    def _create_harvest_objects(self, remote_ids, harvest_job):
-        try:
-            object_ids = []
-            if len(remote_ids):
-                for remote_id in remote_ids:
-                    # Create a new HarvestObject for this identifier
-                    obj = HarvestObject(guid = remote_id, job = harvest_job)
-                    obj.save()
-                    object_ids.append(obj.id)
-                return object_ids
-            else:
-               self._save_gather_error('No remote datasets could be identified', harvest_job)
-        except Exception, e:
-            self._save_gather_error('%r' % e.message, harvest_job)
-
-    def _create_or_update_package(self, package_dict, harvest_object):
-        '''
-        Creates a new package or updates an exisiting one according to the
-        package dictionary provided. The package dictionary should look like
-        the REST API response for a package:
-
-        http://ckan.net/api/rest/package/statistics-catalunya
-
-        Note that the package_dict must contain an id, which will be used to
-        check if the package needs to be created or updated (use the remote
-        dataset id).
-
-        If the remote server provides the modification date of the remote
-        package, add it to package_dict['metadata_modified'].
-
-        '''
-        try:
-            #from pprint import pprint 
-            #pprint(package_dict)
-            ## change default schema
-            schema = default_package_schema()
-            schema["id"] = [ignore_missing, unicode]
-
-            context = {
-                'model': model,
-                'session': Session,
-                'user': u'harvest',
-                'api_version':'2',
-                'schema': schema,
-            }
-
-            # Check if package exists
-            context.update({'id':package_dict['id']})
-            try:
-                existing_package_dict = package_show(context)
-                # Check modified date
-                if not 'metadata_modified' in package_dict or \
-                   package_dict['metadata_modified'] > existing_package_dict.get('metadata_modified'):
-                    log.info('Package with GUID %s exists and needs to be updated' % harvest_object.guid)
-                    # Update package
-                    updated_package = package_update_rest(package_dict, context)
-
-                    harvest_object.package_id = updated_package['id']
-                    harvest_object.save()
-                else:
-                    log.info('Package with GUID %s not updated, skipping...' % harvest_object.guid)
-
-            except NotFound:
-                # Package needs to be created
-                del context['id']
-
-                # Check if name has not already been used
-                package_dict['name'] = self._check_name(package_dict['name'])
-
-                log.info('Package with GUID %s does not exist, let\'s create it' % harvest_object.guid)
-                new_package = package_create_rest(package_dict, context)
-                harvest_object.package_id = new_package['id']
-                harvest_object.save()
-
-            return True
-
-        except ValidationError,e:
-            log.exception(e)
-            self._save_object_error('Invalid package with GUID %s: %r'%(harvest_object.guid,e.error_dict),harvest_object,'Import')
-        except Exception, e:
-            log.exception(e)
-            self._save_object_error('%r'%e,harvest_object,'Import')
-
-        return None
-
-
-class DataPublicaHarvester(PDEUHarvester):
+class DataPublicaHarvester(HarvesterBase):
     INITIAL_INDEX = "http://www.data-publica.com/en/data/WebSection_viewContentDetailledList"
     INDEX_URL = "http://www.data-publica.com/en/data"
 
@@ -170,7 +27,6 @@ class DataPublicaHarvester(PDEUHarvester):
         }
 
     gathered_ids = []
-
     page = 1
 
     def _gather_ids(self,url = None, jar= None):
@@ -222,21 +78,17 @@ class DataPublicaHarvester(PDEUHarvester):
 
         # Get contents
         try:
-            content = self._get_content(url)
+            fh = urllib2.urlopen(url)
+            harvest_object.content = fh.read()
+            harvest_object.save()
+            fh.close()
+            return True
         except Exception,e:
             log.exception(e)
             self._save_object_error('Unable to get content for dataset: %s: %r' % \
                                         (url, e),harvest_object)
-            return None
 
-        # Save the fetched contents in the HarvestObject
-        harvest_object.content = content
-        harvest_object.save()
-
-        return True
-
-    def import_stage(self,harvest_object):
-
+    def import_stage(self, harvest_object):
         log.debug('In DataPublicaHarvester import_stage')
         if not harvest_object:
             log.error('No harvest object received')
@@ -302,8 +154,6 @@ class DataPublicaHarvester(PDEUHarvester):
                         if elem.tail:
                             tag = elem.tail.strip()
                         if tag:
-                            # "Sanitize" tags
-                            tag = re.sub(r'[^a-zA-Z0-9 ]','',tag).replace(' ','-').lower()
                             package_dict['tags'].append(tag)
 
                 if name == 'Description':
@@ -339,7 +189,7 @@ class DataPublicaHarvester(PDEUHarvester):
 
         except Exception, e:
             log.exception(e)
-            self._save_object_error('%r'%e,harvest_object,'Import')
+            self._save_object_error('%r'%e, harvest_object, 'Import')
 
         return self._create_or_update_package(package_dict,harvest_object)
 
@@ -349,7 +199,7 @@ from ckanext.rdf.vocab import Graph
 try: from cStringIO import StringIO
 except ImportError: from StringIO import StringIO
 
-class OpenGovSeHarvester(PDEUHarvester):
+class OpenGovSeHarvester(HarvesterBase):
     INDEX_URL = "http://www.opengov.se/feeds/data/"
     ATOM_NS = "http://www.w3.org/2005/Atom"
 
@@ -374,22 +224,18 @@ class OpenGovSeHarvester(PDEUHarvester):
 
     def fetch_stage(self,harvest_object):
         log.debug('In OpenGovSeHarvester fetch_stage')
-
-        # Get URL
         url = harvest_object.guid.strip('/') + '/rdf/'
-
-        # Get contents
         try:
+            fh = urllib2.urlopen(url)
+            harvest_object.content = fh.read()
+            harvest_object.save()
+            fh.close()
+            return True
             content = self._get_content(url)
-        except Exception,e:
+        except Exception, e:
+            log.exception(e)
             self._save_object_error('Unable to get content for dataset: %s: %r' % \
                                         (url, e), harvest_object)
-            return None
-
-        # Save the fetched contents in the HarvestObject
-        harvest_object.content = content
-        harvest_object.save()
-        return True
 
     def import_stage(self,harvest_object):
         log.debug('In OpenGovSeHarvester import_stage')
@@ -407,12 +253,6 @@ class OpenGovSeHarvester(PDEUHarvester):
 
             url = harvest_object.guid
             package_dict = consume_one(graph)
-            tags = []
-            for tag in package_dict.get('tags', []):
-                tag = re.sub(r'[^a-zA-Z0-9 ]','',tag).replace(' ','-').lower()
-                tags.append(tag)
-            package_dict['tags'] = tags
-
         except Exception, e:
             log.exception(e)
             self._save_object_error('%r'%e,harvest_object,'Import')
@@ -435,7 +275,7 @@ class OpenGovSeHarvester(PDEUHarvester):
 
 import json
 from csv import DictReader
-class DataLondonGovUkHarvester(PDEUHarvester):
+class DataLondonGovUkHarvester(HarvesterBase):
     CATALOGUE_CSV_URL = "http://data.london.gov.uk/datafiles/datastore-catalogue.csv"
 
     def info(self):
@@ -499,8 +339,8 @@ class DataLondonGovUkHarvester(PDEUHarvester):
                         # Common extras
                         'harvest_catalogue_name': 'London Datastore',
                         'harvest_catalogue_url': 'http://data.london.gov.uk',
-                        'eu_country':'UK',
-                        'eu_nuts1':'UKI'
+                        'eu_country': 'UK',
+                        'eu_nuts1': 'UKI'
 
                     },
                     'resources': []
@@ -528,18 +368,12 @@ class DataLondonGovUkHarvester(PDEUHarvester):
 
         package_dict['id'] = harvest_object.guid
         package_dict['name'] = self._gen_new_name(package_dict['title'])
-        tags = []
-        for tag in package_dict.get('tags', []):
-            tag = re.sub(r'[^a-zA-Z0-9 ]','',tag).replace(' ','-').lower()
-            if not tag in tags:
-                tags.append(tag)
-        package_dict['tags'] = tags
         return self._create_or_update_package(package_dict, harvest_object)
 
 
 from lxml import html, etree
 from hashlib import sha1
-class DataWienGvAtHarvester(PDEUHarvester):
+class DataWienGvAtHarvester(HarvesterBase):
     CATALOGUE_FEED_URL = "http://data.wien.gv.at/katalog/.indexR.xml"
 
     def info(self):
@@ -581,12 +415,7 @@ class DataWienGvAtHarvester(PDEUHarvester):
             elif key == 'DC.date.modified':
                 package_dict['metadata_modified'] = value
             elif key == 'keywords':
-                package_dict['tags'] = []
-                for tag in value.split(','):
-                    tag = tag.strip()
-                    tag = re.sub(r'[^a-zA-Z0-9 ]','',tag).replace(' ','-').lower()
-                    if not tag in package_dict['tags']:
-                        package_dict['tags'].append(tag)
+                package_dict['tags'] = value.split(',')
         for row in doc.findall('//table[@class="BDE-table-frame vie-ogd-table"]//tr'):
             key = row.find('th/p').text
             elem = row.find('td')
@@ -653,7 +482,7 @@ class DataWienGvAtHarvester(PDEUHarvester):
             self._save_object_error('%r' % e, harvest_object, 'Import')
 
 
-class OpendataParisFrHarvester(PDEUHarvester):
+class OpendataParisFrHarvester(HarvesterBase):
     PREFIX_URL = "http://opendata.paris.fr/opendata/"
     CATALOGUE_INDEX_URL = "jsp/site/Portal.jsp?page_id=5"
 
@@ -691,11 +520,7 @@ class OpendataParisFrHarvester(PDEUHarvester):
             key = section.text.strip().encode('utf-8')
             value = section.tail.strip().encode('utf-8')
             if 'Mots' in key:
-                for tag in p.findtext('.//span[@id="tags"]').split(','):
-                    tag = tag.strip()
-                    tag = re.sub(r'[^a-zA-Z0-9 ]','',tag).replace(' ','-').lower()
-                    if not tag in package_dict['tags']:
-                        package_dict['tags'].append(tag)
+                package_dict['tags'] = p.findtext('.//span[@id="tags"]').split(',')
             elif 'Description' in key:
                 package_dict['notes'] = value
             elif 'publication' in key:
