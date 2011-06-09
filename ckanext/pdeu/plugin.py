2c2760aec33de8356b4c75d8586f0f3920feabc0
Friedrich Lindenberg <friedrich@pudo.org>
2011-06-09 12:21:39 +0200

    move theme to location mandated by debian packaging, auto-load via IConfigurer

--- ckanext/pdeu/plugin.py
+++ ckanext/pdeu/plugin.py
@@ -1,8 +1,21 @@
-from ckan.plugins import implements, IRoutes, SingletonPlugin
+import os
+from ckan.plugins import implements, SingletonPlugin
+from ckan.plugins import IRoutes, IConfigurer
 
 
 class PDEUCustomizations(SingletonPlugin):
     implements(IRoutes)
+    implements(IConfigurer, inherit=True)
+
+    def update_config(self, config):
+        here = os.path.dirname(__file__)
+        rootdir = os.path.dirname(os.path.dirname(here))
+        our_public_dir = os.path.join(rootdir, 'ckanext', 'pdeu', 'theme', 'public')
+        template_dir = os.path.join(rootdir, 'ckanext', 'pdeu', 'theme', 'templates')
+        config['extra_public_paths'] = ','.join([our_public_dir,
+                config.get('extra_public_paths', '')])
+        config['extra_template_paths'] = ','.join([template_dir,
+                config.get('extra_template_paths', '')])
 
     def before_map(self, route_map):
         wire_controller = 'ckanext.pdeu.controllers:RewiringController'
