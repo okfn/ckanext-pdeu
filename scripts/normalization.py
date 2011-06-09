837df11138ae07ac3bbe6e771dcacd3d43506fbb
Friedrich Lindenberg <friedrich@pudo.org>
2011-06-09 22:44:44 +0200

    categorize packages into groups based on categories

--- scripts/normalization.py
+++ scripts/normalization.py
@@ -2,13 +2,14 @@ from datautil.normalization.table_based import Formats, Licenses
 from datautil.normalization.table_based import Normalizer, NormalizerJoin
 from CREDENTIALS import GOOGLE_USER, GOOGLE_PASS
 from traverse import traverse
+from ckanclient import CkanApiError
 
 from pprint import pprint
 
 def Categories(username, password):
     doc_id = 'tE-DPw3k0uXxgNUk1Q8_U_w'
     first = Normalizer(username, password, doc_id, 'Mapping', 'category')
-    second = Normalizer(username, password, doc_id, 'Groups', 'group_name')
+    second = Normalizer(username, password, doc_id, 'Groups', 'name')
     return NormalizerJoin(first, second)
 
 
@@ -34,6 +35,20 @@ class categories(object):
 
     def __init__(self):
         self.normalizer = Categories(GOOGLE_USER, GOOGLE_PASS)
+    
+    def _assign_group(self, client, pkg_name, data):
+        try:
+            group = client.group_entity_get(data.get('name'))
+            if pkg_name not in group['packages']:
+                group['packages'] += [pkg_name]
+                client.group_entity_put(group)
+        except CkanApiError, e:
+            group = {'name': data.get('name'),
+                     'title': data.get('title', data.get('name')),
+                     'description': data.get('description', ''),
+                     'packages': [pkg_name]
+                     }
+            client.group_register_post(group)
 
     def __call__(self, client, pkg):
         cats = pkg.get('extras', {}).get('categories', [])
@@ -41,9 +56,9 @@ class categories(object):
             cats = [cats]
         for cat in cats:
             data = self.normalizer.get(cat, source_hint=pkg.get('ckan_url'))
-            if data.get('group_name'):
-                pprint(data)
-        #return pkg
+            pprint(data)
+            if data.get('name'):
+                self._assign_group(client, pkg.get('name'), data)
 
 if __name__ == '__main__':
     traverse(categories())
