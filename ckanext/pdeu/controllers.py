cf0439a5edb87a1c0ea59165149c533b1156f515
Sean Hammond <seanhammond@lavabit.com>
2012-10-08 11:25:48 +0200

    controllers.py whitespace fixes
    Make syntastic happy (and the code much more readable)

--- ckanext/pdeu/controllers.py
+++ ckanext/pdeu/controllers.py
@@ -19,10 +19,12 @@ log = logging.getLogger(__name__)
 from datetime import datetime
 import gdata.spreadsheet.text_db
 
+
 def get_root_dir():
     here = os.path.dirname(__file__)
     rootdir = os.path.dirname(os.path.dirname(here))
     return rootdir
+
 
 class RewiringController(BaseController):
 
@@ -40,15 +42,16 @@ class SubscribeController(BaseController):
             * pdeu.gdocs.dockey
             * pdeu.gdocs.sheet [Optional, defaults to 'Sheet1'
 
-        The spreadhsheet must have two header fields named 'email' and 'signedup'
-    '''
+        The spreadhsheet must have two header fields named 'email' and
+        'signedup'
 
+    '''
     def __before__(self):
         # Check Google Docs parameters
-        username = config.get('pdeu.gdocs.username',None)
-        password = config.get('pdeu.gdocs.password',None)
-        dockey = config.get('pdeu.gdocs.dockey',None)
-        sheet = config.get('pdeu.gdocs.sheet','Sheet1')
+        username = config.get('pdeu.gdocs.username', None)
+        password = config.get('pdeu.gdocs.password', None)
+        dockey = config.get('pdeu.gdocs.dockey', None)
+        sheet = config.get('pdeu.gdocs.sheet', 'Sheet1')
 
         if not username or not password or not dockey:
             log.error('Google Docs connection settings not specified')
@@ -56,40 +59,42 @@ class SubscribeController(BaseController):
 
         # Setup connection
         self.client = gdata.spreadsheet.text_db.DatabaseClient(
-            username=username,password=password)
+            username=username, password=password)
         db = self.client.GetDatabases(dockey)[0]
         self.table = db.GetTables(name=sheet)[0]
         self.table.LookupFields()
 
     def send(self):
         if not 'email' in request.params:
-            abort(400,_('Please provide an email address'))
+            abort(400, _('Please provide an email address'))
         email = request.params['email']
-        row = {'email':email,'signedup': datetime.now().isoformat()}
+        row = {'email': email, 'signedup': datetime.now().isoformat()}
         self.table.AddRecord(row)
-        h.flash_success(_('Your email has been stored. Thank you for your interest.'))
+        h.flash_success(_(
+            'Your email has been stored. Thank you for your interest.'))
         redirect('/')
 
+
 class MapController(BaseController):
-    
+
     def _get_config(self):
-        c.startColor = config.get('pdeu.map.start_color','#FFFFFF')
-        c.endColor = config.get('pdeu.map.end_color','#045A8D')
+        c.startColor = config.get('pdeu.map.start_color', '#FFFFFF')
+        c.endColor = config.get('pdeu.map.end_color', '#045A8D')
         c.num_groups = config.get('pdeu.map.groups', 5)
 
     def index(self):
         self._get_config()
-        
+
         # package search
         context = {'model': model, 'session': model.Session,
-                   'user': c.user or c.author,'for_view':True}
+                   'user': c.user or c.author, 'for_view': True}
         data_dict = {
-            'q':'*:*',
-            'facet.field':g.facets,
-            'rows':0,
-            'start':0,
+            'q': '*:*',
+            'facet.field': g.facets,
+            'rows': 0,
+            'start': 0,
         }
-        query = get_action('package_search')(context,data_dict)
+        query = get_action('package_search')(context, data_dict)
         c.package_count = query['count']
         c.facets = query['facets']
         c.search_facets = query['search_facets']
@@ -100,18 +105,18 @@ class MapController(BaseController):
         self._get_config()
         return render('home/map.html')
 
-
     def data(self):
         # Get the Europe dataset
         rootdir = get_root_dir()
         data_file = os.path.join(rootdir, 'ckanext', 'pdeu', 'data', 'eu.json')
-        f = open(data_file,'r')
+        f = open(data_file, 'r')
         o = json.load(f)
 
         # Get the package count by country
-        q = Session.query(distinct(PackageExtra.value), func.count(PackageExtra.value)) \
-                   .filter(PackageExtra.key==u'eu_country') \
-                   .group_by(PackageExtra.value)
+        q = Session.query(distinct(PackageExtra.value),
+                func.count(PackageExtra.value)).filter(
+                        PackageExtra.key == u'eu_country').group_by(
+                                PackageExtra.value)
         values = {}
         for country, count in q.all():
             values[country] = count
@@ -119,13 +124,11 @@ class MapController(BaseController):
         # Set the package count for each country
         for ft in o['features']:
             code = ft['properties']['NUTS']
-            ft['properties']['packages'] = values[code] if code in values else 0
+            ft['properties']['packages'] = (
+                    values[code] if code in values else 0)
 
         response.content_type = 'application/json'
-        response.pragma = None 
+        response.pragma = None
         response.cache_control = 'public; max-age: 3600'
         response.cache_expires(seconds=3600)
         return json.dumps(o)
-
-
-
